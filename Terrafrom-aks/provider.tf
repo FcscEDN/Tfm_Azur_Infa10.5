46d5180480bf52f081fecc4c4d4c8cec94313093
DhineshKumarInfa <106745440+DhineshKumarInfa@users.noreply.github.com>
2022-06-08 13:33:37 +0530

    updated azure credentials
    updated azure credentials

--- Terrafrom-aks/provider.tf
+++ Terrafrom-aks/provider.tf
@@ -1,4 +1,9 @@
 provider "azurerm" {
+  subscription_id = ${{secrets.AZURE_SUBSCRIPTION_ID}}
+  client_id       = ${{secrets.AZURE_CLIENT_ID}}
+  client_secret   = ${{secrets.AZURE_CLIENT_SECRET}}
+  tenant_id       = ${{secrets.AZURE_TENANT_ID}}
+  version         = "=2.0.0" #Can be overide as you wish
   features {}
 }
 
