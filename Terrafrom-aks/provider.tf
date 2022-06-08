3da87a6a1f961601f7a72d9c045a7bcd7cbd5ae4
DhineshKumarInfa <106745440+DhineshKumarInfa@users.noreply.github.com>
2022-06-08 13:37:20 +0530

    Update provider.tf
    var

--- Terrafrom-aks/provider.tf
+++ Terrafrom-aks/provider.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-  subscription_id = ${{secrets.AZURE_SUBSCRIPTION_ID}}
-  client_id       = ${{secrets.AZURE_CLIENT_ID}}
-  client_secret   = ${{secrets.AZURE_CLIENT_SECRET}}
-  tenant_id       = ${{secrets.AZURE_TENANT_ID}}
+  subscription_id = var.SUBSCRIPTION_ID
+  client_id       = var.SP_CLIENT_ID
+  client_secret   = var.SP_CLIENT_SECRET
+  tenant_id       = var.SP_TENANT_ID
   version         = "=2.0.0" #Can be overide as you wish
   features {}
 }
